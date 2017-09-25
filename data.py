236b5c11e1016609e8d397bf57785932bda4dfb5
Max <maxq210@gmail.com>
2017-09-25 11:17:07 -0700

    none

--- data.py
+++ data.py
@@ -86,7 +86,8 @@ def _reshape_batch(inputs, size, batch_size):
     """
     batch_inputs = []
     for length_id in range(size):
-        if type(batch_size) is 'NoneType':
+        if batch_size is None:
+            print "hello\n"
             batch_size = 0
         batch_inputs.append(np.array([inputs[batch_id][length_id]
                                     for batch_id in range(batch_size)], dtype=np.int32))
